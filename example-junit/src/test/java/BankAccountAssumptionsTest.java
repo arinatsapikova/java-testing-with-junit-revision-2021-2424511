1f823decc66c1cee428d188ad4a790b47eb55bc1
arinatsapikova <arina.tsapikova@gmail.com>
2023-01-22 10:59:18 -0600

    initial commit

--- example-junit/src/test/java/BankAccountAssumptionsTest.java
+++ example-junit/src/test/java/BankAccountAssumptionsTest.java
@@ -1,13 +1,16 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 public class BankAccountAssumptionsTest {
     @Test
     @DisplayName("Test activation account after creation")
     public void testActive() {
         BankAccount bankAccount = new BankAccount(500, 0);
-        assertTrue(bankAccount.isActive());
+
+      assumingThat(bankAccount == null, ()->   assertTrue(bankAccount.isActive()));
+
     }
 }
